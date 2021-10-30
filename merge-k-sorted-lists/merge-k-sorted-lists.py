1ee5f854ca2fd72ee57a76fdf12bb85b2800eafd
Tirth Shroff <54340408+tirth2801@users.noreply.github.com>
2021-10-30 19:03:41 -0400

    Time: 88 ms (98.60%), Space: 18.2 MB (33.90%) - LeetHub

---
+++ merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        # Brute Force Approach: 
+        # - Traverse all the linked lists and collect the values of the nodes into an array.
+        # - Sort and iterate over this array to get the proper value of nodes.
+        # - Create a new sorted linked list and extend it with the new nodes.
+        
+        self.nodes = []
+        head = point = ListNode(0)
+        for l in lists:
+            while l:
+                self.nodes.append(l.val)
+                l = l.next
+        for x in sorted(self.nodes):
+            point.next = ListNode(x)
+            point = point.next
+        return head.next
+        
