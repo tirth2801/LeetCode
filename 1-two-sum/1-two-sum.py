56d050acb5911816648e15ed274844b269516db9
Tirth Shroff <54340408+tirth2801@users.noreply.github.com>
2022-01-31 17:07:14 -0500

    Time: 60 ms (85.71%), Space: 15.4 MB (41.04%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i in range(len(nums)):
+            if nums[i] not in map:
+                map[target - nums[i]] = i
+            else:
+                return map[nums[i]], i
+        return None
+            
+        
